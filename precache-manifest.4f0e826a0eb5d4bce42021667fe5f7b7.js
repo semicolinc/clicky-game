4f3802e039a72a61ac02eb09fa3d4ed15a5c5fef
colchang <colchang@iu.edu>
2020-06-10 18:29:28 -0700

    Updates

---
+++ precache-manifest.4f0e826a0eb5d4bce42021667fe5f7b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed77e363c6c32ee900c0a5f8dda45fa",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "abb7099bcc1c21a84840",
+    "url": "/clicky-game/static/css/main.7ba16cad.chunk.css"
+  },
+  {
+    "revision": "f3174129e566c9785c64",
+    "url": "/clicky-game/static/js/2.ee8ccc7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.ee8ccc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb7099bcc1c21a84840",
+    "url": "/clicky-game/static/js/main.63d84386.chunk.js"
+  },
+  {
+    "revision": "4b0c67636a906e3a9dcf",
+    "url": "/clicky-game/static/js/runtime-main.ae96167f.js"
+  },
+  {
+    "revision": "f0e512c7ee222713e46e764784c7bc23",
+    "url": "/clicky-game/static/media/logo.f0e512c7.svg"
+  }
+]);
